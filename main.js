b8e31ae07bfb1b0449c77439aa4cc8ba75bdf9c8
Lyfhael <singinganthlers@hotmail.com>
2023-12-07 20:48:06 +0100

    Update main.js
    added from_archive option

--- main.js
+++ main.js
@@ -10,8 +10,17 @@ var tweets_to_delete = []
 var user_id = getCookie("twid").substring(4);
 var username = "YourUsernameHere" // replace with your username
 var stop_signal = undefined
+var twitter_archive_content = undefined
+var twitter_archive_loading_confirmed = false
 
 var delete_options = {
+	/*  from_archive: If you downloaded your archive from Twitter, set this to true.
+	    You will be prompt to upload the tweets.js file from it.
+		Advantage is that this is much more reliable and faster.
+		You can combine this with options: unretweet, match_any_keywords, tweets_to_ignore, after/before date
+		others will be ignored
+	 */
+	"from_archive":false,
 	/*  unretweet: seems obvious, but it unretweet if set to true */
 	"unretweet":false,
 	/* do_not_remove_pinned_tweet: THIS CAN FAIL. Twitter has too many different way to format their response that I cannot guarantee this to work 100%
@@ -199,6 +208,18 @@ function check_date(tweet) {
 	return true
 }
 
+function check_date_archive(created_at) {
+	tweet_date = new Date(created_at)
+	tweet_date.setHours(0, 0, 0, 0);
+	if (tweet_date > delete_options["after_date"] && tweet_date < delete_options["before_date"]) {
+		return true
+	}
+	else if (tweet_date < delete_options["after_date"]) {
+		stop_signal = true
+	}
+	return false
+}
+
 function check_filter(tweet) {
 	if (tweet['legacy'].hasOwnProperty('id_str')
 		&& ( delete_options["tweets_to_ignore"].includes(tweet['legacy']["id_str"]) || delete_options["tweets_to_ignore"].includes( parseInt(tweet['legacy']["id_str"]) ) )) {
@@ -217,6 +238,18 @@ function check_filter(tweet) {
 	return false
 }
 
+function check_filter_archive(tweet_obj) {
+	let tweet_id = tweet_obj["id"]
+	let tweet_str = tweet_obj["text"]
+	let tweet_date = tweet_obj["date"]
+	if ((delete_options["tweets_to_ignore"].includes(tweet_id) || delete_options["tweets_to_ignore"].includes( parseInt(tweet_id) ) )) {
+		return false
+	}
+	if (check_keywords(tweet_str) && check_date_archive(tweet_date))
+		return true
+	return false
+}
+
 function check_tweet_owner(obj, uid) {
 	if (obj.hasOwnProperty('legacy') && obj['legacy'].hasOwnProperty('retweeted') && obj['legacy']['retweeted'] === true && delete_options["unretweet"] == false)
 		return false
@@ -229,6 +262,38 @@ function check_tweet_owner(obj, uid) {
 
 function tweetFound(obj) {
 	console.log(`found ${obj['legacy']['full_text']}`)
+}
+
+function parseTweetsFromArchive(data) {
+    try {
+        const filteredIds = [];
+
+        data.forEach(item => {
+            if (item.tweet && item.tweet.id_str) {
+                const isInReplyToExcludedUser = item.tweet.in_reply_to_user_id_str === user_id;
+                const startsWithRT = item.tweet.full_text.startsWith('RT ');
+				
+				let tweet_obj = {}
+				tweet_obj["id"] = item.tweet.id_str
+				tweet_obj["text"] = item.tweet.full_text
+				tweet_obj["date"] = item.tweet.created_at
+                if (!isInReplyToExcludedUser && (delete_options["unretweet"] == startsWithRT)
+					&& check_filter_archive(tweet_obj)) {
+					;
+				}
+				else {
+					return;
+				}
+				console.log("DELETING:",item.tweet.full_text)
+				filteredIds.push(item.tweet.id_str);
+            }
+        });
+
+        return filteredIds;
+    } catch (error) {
+        console.error("Error parsing JSON:", error);
+        return [];
+    }
 }
 
 function findTweetIds(obj) {
@@ -299,7 +364,7 @@ async function delete_tweets(id_list) {
 				i -= 1;
 				continue
 			}
-			if (retry == 5) {
+			if (retry == 8) {
 				throw new Error("Max retries reached")
 			}
 			console.log(response.text())
@@ -317,7 +382,250 @@ async function delete_tweets(id_list) {
 var next = null
 var entries = undefined
 
-if (delete_options["delete_specific_ids_only"].length == 1 && delete_options["delete_specific_ids_only"][0].length == 0) {
+if (delete_options["from_archive"] == true) {
+	console.log("Waiting for user to load his Twitter archive")
+
+    // Create modal elements
+    const modal = document.createElement('div');
+    modal.id = 'myModal';
+    modal.className = 'modal';
+
+    const modalContent = document.createElement('div');
+    modalContent.className = 'modal-content';
+
+    const closeSpan = document.createElement('span');
+    closeSpan.className = 'close';
+    closeSpan.innerHTML = '&times;';
+
+    const header = document.createElement('h2');
+    header.innerText = 'Drop Your File Here';
+
+    const dropArea = document.createElement('div');
+    dropArea.id = 'drop-area';
+    dropArea.className = 'drop-area';
+    dropArea.innerHTML = '<p>Drop your tweets.js from your Twitter Archive here</p>';
+
+    // Append elements
+    modalContent.appendChild(closeSpan);
+    modalContent.appendChild(header);
+    modalContent.appendChild(dropArea);
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal);
+
+    // Add CSS styles
+    const styles = `
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0,0,0,0.4);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .modal-content {
+            background-color: #fff;
+            margin: auto;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+            width: 400px;
+            text-align: center;
+        }
+        .close {
+            color: #aaa;
+            position: absolute;
+            top: 10px;
+            right: 20px;
+            font-size: 24px;
+            font-weight: bold;
+            cursor: pointer;
+        }
+        .close:hover {
+            color: black;
+        }
+        .drop-area {
+            border: 2px dashed #007bff;
+            border-radius: 5px;
+            padding: 60px;
+            cursor: pointer;
+            transition: .5s ease-in-out;
+        }
+        .drop-area:hover {
+            border-color: #0056b3;
+			background-color: #dff3fb;
+			transition: .5s ease-in-out;
+        }
+		.drop-area.active {
+			background-color: #f3f4f6; /* Lighter background */
+			border-color: #4caf50; /* Green border */
+			color: #4caf50; /* Green text */
+		}
+		
+		.drop-area.active p {
+			font-weight: bold;
+			color: #4caf50;
+		}
+        h2 {
+            color: #333;
+            margin-bottom: 20px;
+        }
+        p {
+            margin: 0;
+            color: #666;
+        }
+		confirm-button {
+			margin-top: 30px;
+			background-color: rgb(0, 116, 212);
+			border: 2px solid rgb(0, 116, 212);
+			border-radius: 3px;
+		}
+    `;
+
+    const styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
+
+    // Display modal
+    modal.style.display = 'flex';
+
+    // Close modal on click
+    closeSpan.onclick = function() {
+        modal.style.display = 'none';
+    };
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    };
+	const confirmButton = document.createElement('button');
+	confirmButton.innerText = 'Confirm';
+	confirmButton.className = 'confirm-button';
+	confirmButton.style.marginTop = "5px"
+
+	// Append confirm button to modal content
+	modalContent.appendChild(confirmButton);
+
+	// Confirm button event listener
+	confirmButton.addEventListener('click', () => {
+		if (twitter_archive_content) {
+			console.log("Confirmation received. File processed.");
+			twitter_archive_loading_confirmed = true
+			modal.style.display = 'none';
+			// Further processing can be done here
+		} else {
+			console.error("No file loaded. Please load a file before confirming.");
+		}
+	});
+    // Drag and Drop functionality
+    dropArea.addEventListener('dragover', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'copy';
+        dropArea.style.borderColor = '#0056b3';
+    });
+
+    dropArea.addEventListener('dragleave', (event) => {
+        dropArea.style.borderColor = '#007bff';
+    });
+
+    dropArea.addEventListener('drop', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        dropArea.style.borderColor = '#007bff';
+        const files = event.dataTransfer.files;
+
+        // Process file here
+        console.log(files[0]);
+    });
+
+    // Click to upload functionality
+    dropArea.onclick = function() {
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.onchange = e => {
+            // Process file here
+            console.log(e.target.files[0]);
+        };
+        fileInput.click();
+    };
+	function readFile(file) {
+		const reader = new FileReader();
+		reader.onload = function(event) {
+			const content = event.target.result;
+	
+			// Split by '=' and remove the first part
+			const parts = content.split('=');
+			parts.shift(); // Remove the first element
+			const jsonPart = parts.join('=').trim(); // Rejoin the rest and trim
+	
+			try {
+				const data = JSON.parse(jsonPart);
+				twitter_archive_content = data;
+				console.log("JSON data loaded into global variable.");
+			} catch (e) {
+				console.error("Error parsing JSON:", e);
+			}
+		};
+		reader.onerror = function(error) {
+			console.error("Error reading file:", error);
+		};
+		reader.readAsText(file); // Read the file as text
+	}
+
+    // Modify the drop event
+    dropArea.addEventListener('drop', (event) => {
+        // ... [existing event handler code] ...
+        const file = event.dataTransfer.files[0];
+        readFile(file);
+    });
+
+    // Modify the file input change event
+    dropArea.onclick = function() {
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.onchange = e => {
+            const file = e.target.files[0];
+            readFile(file);
+        };
+        fileInput.click();
+    };
+	dropArea.addEventListener('dragover', (event) => {
+		event.stopPropagation();
+		event.preventDefault();
+		event.dataTransfer.dropEffect = 'copy';
+		dropArea.classList.add('active'); // Add 'active' class
+	});
+	
+	dropArea.addEventListener('dragleave', (event) => {
+		dropArea.classList.remove('active'); // Remove 'active' class
+	});
+	
+	dropArea.addEventListener('drop', (event) => {
+		event.stopPropagation();
+		event.preventDefault();
+		dropArea.classList.remove('active'); // Remove 'active' class
+		// Rest of your drop event code...
+	});
+}
+
+if (delete_options["from_archive"] == true) {
+	while (twitter_archive_loading_confirmed == false) {
+		await sleep(1000)
+	}
+	tweets_to_delete = parseTweetsFromArchive(twitter_archive_content)
+	console.log(tweets_to_delete)
+	await delete_tweets(tweets_to_delete)
+}
+
+
+else if (delete_options["delete_specific_ids_only"].length == 1 && delete_options["delete_specific_ids_only"][0].length == 0) {
 	while (next != "finished" && stop_signal != true) {
 		entries = await fetch_tweets(next);
 		next = await log_tweets(entries);
